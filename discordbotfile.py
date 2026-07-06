8a3a96afab8000586436a0646e299a6b661382e6
ascension-toheaven <7658potato@gmail.com>
2026-07-05 21:24:29 -0400

    code finally

---
+++ discordbotfile.py
@@ -0,0 +1,8 @@
+import discord
+print(discord.__version__)
+print("yo lmao")
+print("where is my code")
+#sus where is my code
+#ideas: reminds u to do stuff, tracking stuff outside of discord (like github or like yt), sends random stuff, idea gen
+# reaction roles 
+#lwk bot that acts like me drooling emoji ts js an llm
