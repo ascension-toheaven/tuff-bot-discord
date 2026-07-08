47b5bd2421f82f622f7108ac2ab4f8a122e60d3a
ascension-toheaven <7658potato@gmail.com>
2026-07-08 13:45:46 -0400

    added actual basic code: message to send at init, and 1 command to send a message + take user input
    3rd time's the charm

--- discordbotfile.py
+++ discordbotfile.py
@@ -1,7 +1,25 @@
 import discord
-print(discord.__version__)
-print("yo lmao")
-print("where is my code")
+from discord.ext import commands
+
+tufftoken = "MTUyMjMxNzA5NjgxNjc0MjU3MQ.Gaxt_h.qHwMuito41l1h8HsqpsycVLncT9rNGfkCIj59E"
+channelid = 1524459268852551892
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("yo sus")
+    channel = bot.get_channel(channelid)
+    await channel.send("yo hello sus")
+
+@bot.command()
+async def sus(ctx, a, b):
+    ts = a + b
+    await ctx.send("ayo" + ts)
+
+bot.run(tufftoken)
+
 #sus where is my code
 #ideas: reminds u to do stuff, tracking stuff outside of discord (like github or like yt), sends random stuff, idea gen
 # reaction roles 
